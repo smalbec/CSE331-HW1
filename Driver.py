a7b08aac07a7a985407ec2e0115c94e581bfa78c
smalbec <44180894+smalbec@users.noreply.github.com>
2020-02-14 18:30:29 -0500

    FIND A WAY TO GET THE MATCH WHERE THE MAN IS FUCK

--- Driver.py
+++ Driver.py
@@ -9,13 +9,14 @@ class Driver:
             print("Please provide the testcase filepath as a command line argument")
             return
         input = Utility().read_file(sys.argv[1])
+        #print(input)
         s = Solution(len(input.women_preferences), input.women_preferences, input.men_preferences)
         sol = s.output_stable_matchings()
         for match in sol: #Sort by women 
             match.sort()
         sol.sort() #sorting across differnt matchings
-        print(len(sol))
-        for m in sol:
-            print(m)
+        #print(len(sol))
+        #for m in sol:
+        #   print(m)
 
 Driver()
--- Solution.py
+++ Solution.py
@@ -1,5 +1,7 @@
 from Marriage import Marriage
 
+import itertools
+
 
 class Solution:
 
@@ -17,8 +19,66 @@ class Solution:
         self.stable_matchings = []
 
     def output_stable_matchings(self):
+
+        #marriage = Marriage(3,)
+
+        #a = itertools.permutations()
+
+        print(self.women)
+
+        all_perfect_matching = []
+
+        #Creates all permutations of perfect matchings
+        for i in list(itertools.permutations(range(1,self.num+1),self.num)):
+            perfect_matching = []
+            numm = 1
+            for j in i:
+                if(numm < self.num+1):
+                    perfect_matching.append((numm,j))
+                    numm += 1        
+            for match in perfect_matching:
+                #prefference is the index of the matched man in the woman prefference list
+                #if not zero it means there are men the woman preffers
+                #Basically checks if any more preffered men exist in her list
+                prefference = (self.woman[match[0]]).index(match[1])
+                while(prefference != 0):
+                    prefference += -1
+                #Checks index of the woman looking for better match in men prefference list
+                #We want to check if it's index is higher or lower than the one from the man's matched woman    
+                perfect_matching
+
+
+                #Loop goes through the preference list of the current woman in reverse (1,2,3) so it starts with 3
+                for prefference in reversed(self.women[match[1]]):
+                    if(match[1] < preference):
+                #Loop goes through the preference list of the current men in reverse (1,2,3) so it starts with 3
+                        
+
+
+git commit -a -m "FIND A WAY TO GET THE MATCH WHERE THE MAN IS FUCK"
+
+        #all_perfect_matching.append(perfect_matching)
+                
+
+        print(all_perfect_matching)
+
+        
+        
+        #print(list(itertools.permutations("ABC",2)))
+
+        #print(list(itertools.permutations(range(value),3))))
+
+
+        #for key, value in self.women.items():
+            #for key, value in self.men.items():
+            #perfect_matching.append(list(itertools.permutations(range(1,4),2)))
+
+        #print(perfect_matching)
+
+
         """
         This method both computes and returns the stable matchings
-        :return: the list of stable matchings
+        :return: the list of stable matchings   
         """
+
         return self.stable_matchings
